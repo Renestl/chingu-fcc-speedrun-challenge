5a5745a595fde4a75ec8b8fea97671e94e7cc78a
Jennifer Currie <renestl@gmail.com>
2017-05-08 23:57:59 -0500

    setup basic structure

--- data-vis/recipe-box/src/app/components/app.component.ts
+++ data-vis/recipe-box/src/app/components/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 
+import { Recipe } from './../model/recipe';
+
+const RECIPES: Recipe[] = [
+  {name: "recipe 1"},
+  {name: "recipe 2"},
+  {name: "recipe 3"},
+];
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Recipe Box';
+  recipes = RECIPES;
 }
